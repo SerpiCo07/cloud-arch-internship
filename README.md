e1bc9e5e44e38b8d9372b68905348193f4c2a270
devboy <mohammadtayebi@protonmail.com>
2024-01-22 16:55:28 +0100

    Update on main_script and readme

--- README.md
+++ README.md
@@ -21,4 +21,19 @@ In order to avoid hardcoding the path of the Key in our code, it'd be better if 
 ```
 Instead, in script we just read the environment variable.
 
-## What is Workload Identity Federation
+### storage_client_package
+This package is made of two python files 
+    - google_cloud_storage_manager.py 
+         contains the GoogleCloudStorageManager class which encapsulates all the methods that are required to create a bucket and upload/download from it
+
+    - main_script.py 
+        - Where an instance of the oogleCloudStorageManager class is created to do one or more operations
+        - The one we customize and run each time according to our needs
+
+## How to notify the API Gateway a new file is uploaded in the cloud storage
+
+The thing right now I am thinking about is having uploaded a file on our bucket , how should we notify the api gateway ?
+
+Firstly, I'd check if GCS does have any integrated feature to help us with this matter
+
+On the other hand,I'm thinking of creating a cloud storage triggered fucntion to hand over the files to the api gateway
--- storage_client_pack/main_script.py
+++ storage_client_pack/main_script.py
@@ -3,21 +3,21 @@ import logging
 from storage_client_pack.google_cloud_storage_manager import GoogleCloudStorageManager
 
 def main():
-    credentials_path = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS', '/path/to/credentials.json')
+    credentials_path = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS', '/home/devboy/Downloads/mainproject-01-d30cc6b753a8.json')
     gcs_manager = GoogleCloudStorageManager(credentials_path)
 
-    bucket_name = 'python-bucket-demo'
-
+    bucket_name = 'test-app-esteco'
+    """
     # Create a bucket
     gcs_manager.create_bucket(bucket_name)
-
+    """
     # Upload a file
-    file_path = '/home/devboy/Downloads/waterData.zip'
+    file_path = '/home/devboy/Downloads/waterDat.zip'
     gcs_manager.upload_to_bucket('water data', file_path, bucket_name)
-
+    """
     # Download a file
-    download_path = os.path.join(os.getcwd(), 'waterData.zip')
+    download_path = os.path.join(os.getcwd(), 'waterDat.zip')
     gcs_manager.download_from_bucket('water data', download_path, bucket_name)
-
+    """
 if __name__ == "__main__":
     main()
